014def58491f35f4e4bcfdc8aba49b8eaba27a2d
andreasschenkel <31856043+andreasschenkel@users.noreply.github.com>
2020-10-05 20:04:23 +0200

    MDL-69856 assignfeedback_pdf: support annotation of png-files
    Support annotation of png-files in feedback of assignments

--- mod/assign/feedback/editpdf/classes/document_services.php
+++ mod/assign/feedback/editpdf/classes/document_services.php
@@ -58,8 +58,10 @@ class document_services {
     const COMBINED_PDF_FILENAME = 'combined.pdf';
     /**  Temporary place to save JPG Image to PDF file */
     const TMP_JPG_TO_PDF_FILEAREA = 'tmp_jpg_to_pdf';
+    /**  Temporary place to save PNG Image to PDF file */
+    const TMP_PNG_TO_PDF_FILEAREA = 'tmp_png_to_pdf';
     /**  Temporary place to save (Automatically) Rotated image eg JPG FILE */
-    const TMP_ROTATED_JPG_FILEAREA = 'tmp_rotated_image';
+    const TMP_ROTATED_FILEAREA = 'tmp_rotated_image';
     /** Hash of blank pdf */
     const BLANK_PDF_HASH = '4c803c92c71f21b423d13de570c8a09e0a31c718';
 
@@ -205,6 +207,20 @@ EOD;
                             // Save as PDF file if there is no available converter.
                             if (!$converter->can_convert_format_to('jpg', 'pdf')) {
                                 $pdffile = self::save_jpg_to_pdf($assignment, $userid, $attemptnumber, $file, $size);
+                                if ($pdffile) {
+                                    $files[$filename] = $pdffile;
+                                }
+                            }
+                        } else if ($plugin->allow_image_conversion() && $mimetype === "image/png") {
+                            // Rotates image based on the EXIF value.
+                            list ($rotateddata, $size) = $file->rotate_image();
+                            if ($rotateddata) {
+                                $file = self::save_rotated_image_file($assignment, $userid, $attemptnumber,
+                                    $rotateddata, $filename);
+                            }
+                            // Save as PDF file if there is no available converter.
+                            if (!$converter->can_convert_format_to('png', 'pdf')) {
+                                $pdffile = self::save_png_to_pdf($assignment, $userid, $attemptnumber, $file, $size);
                                 if ($pdffile) {
                                     $files[$filename] = $pdffile;
                                 }
@@ -1044,6 +1060,59 @@ EOD;
         }
         return $pdffile;
     }
+    
+    
+     /**
+      * Convert png file to pdf file
+      * @param int|\assign $assignment Assignment
+      * @param int $userid User ID
+      * @param int $attemptnumber Attempt Number
+      * @param \stored_file $file file to save
+      * @param null|array $size size of image
+      * @return \stored_file
+      * @throws \file_exception
+      * @throws \stored_file_creation_exception
+      */
+    private static function save_png_to_pdf($assignment, $userid, $attemptnumber, $file, $size=null) {
+        // Temporary file.
+        $filename = $file->get_filename();
+        $tmpdir = make_temp_directory('assignfeedback_editpdf' . DIRECTORY_SEPARATOR
+            . self::TMP_PNG_TO_PDF_FILEAREA . DIRECTORY_SEPARATOR
+            . self::hash($assignment, $userid, $attemptnumber));
+        $tempfile = $tmpdir . DIRECTORY_SEPARATOR . $filename . ".pdf";
+        // Determine orientation.
+        $orientation = 'P';
+        if (!empty($size['width']) && !empty($size['height'])) {
+            if ($size['width'] > $size['height']) {
+                $orientation = 'L';
+            }
+        }
+        // Save PNG image to PDF file.
+        $pdf = new pdf();
+        $pdf->SetHeaderMargin(0);
+        $pdf->SetFooterMargin(0);
+        $pdf->SetMargins(0, 0, 0, true);
+        $pdf->setPrintFooter(false);
+        $pdf->setPrintHeader(false);
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+        $pdf->AddPage($orientation);
+        $pdf->SetAutoPageBreak(false);
+        // Width has to be define here to fit into A4 page. Otherwise the image will be inserted with original size.
+        if ($orientation == 'P') {
+            $pdf->Image('@' . $file->get_content(), 0, 0, 210);
+        } else {
+            $pdf->Image('@' . $file->get_content(), 0, 0, 297);
+        }
+        $pdf->setPageMark();
+        $pdf->save_pdf($tempfile);
+        $filearea = self::TMP_PNG_TO_PDF_FILEAREA;
+        $pdffile = self::save_file($assignment, $userid, $attemptnumber, $filearea, $tempfile);
+        if (file_exists($tempfile)) {
+            unlink($tempfile);
+            rmdir($tmpdir);
+        }
+        return $pdffile;
+    }
 
     /**
      * Save rotated image data to file.
@@ -1057,7 +1126,7 @@ EOD;
      * @throws \stored_file_creation_exception
      */
     private static function save_rotated_image_file($assignment, $userid, $attemptnumber, $rotateddata, $filename) {
-        $filearea = self::TMP_ROTATED_JPG_FILEAREA;
+        $filearea = self::TMP_ROTATED_FILEAREA;
         $tmpdir = make_temp_directory('assignfeedback_editpdf' . DIRECTORY_SEPARATOR
             . $filearea . DIRECTORY_SEPARATOR
             . self::hash($assignment, $userid, $attemptnumber));
